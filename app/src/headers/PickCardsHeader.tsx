0f0084dcddcfdbd8eab61376ca505eb25763883c
Patrick Beaume <p.beaume@gmail.com>
2025-08-02 10:56:43 +0200

    Display PlayCardHeader when there is one card on the table

--- app/src/headers/PickCardsHeader.tsx
+++ app/src/headers/PickCardsHeader.tsx
@@ -1,9 +1,12 @@
 /** @jsxImportSource @emotion/react */
 
+import { LocationType } from '@gamepark/odin/material/LocationType'
+import { MaterialType } from '@gamepark/odin/material/MaterialType'
 import { PlayCardsRule } from '@gamepark/odin/rules/PlayCardsRule'
 import { useGame, usePlayerId, usePlayerName } from '@gamepark/react-game'
 import { MaterialGame } from '@gamepark/rules-api'
 import { Trans } from 'react-i18next'
+import { PlayCardsHeader } from './PlayCardsHeader'
 
 export const PickCardsHeader = () => {
   const game = useGame<MaterialGame>()!
@@ -12,6 +15,10 @@ export const PickCardsHeader = () => {
   const activePlayer = rule.player
   const itsMe = playerId && playerId === activePlayer
   const name = usePlayerName(activePlayer)
+
+  const currentTable = rule.material(MaterialType.Card).location(LocationType.MiddleOfTable).length
+
+  if (currentTable === 1) return <PlayCardsHeader />
 
   if (itsMe) {
     return <Trans defaults="header.take"></Trans>
